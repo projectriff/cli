78cb10a4827b698ea6fef248d6aca29ca28b29cf
Scott Andrews <sandrews@pivotal.io>
2019-06-21 12:58:41 -0400

    Bump FATS
    - use duffle k8s driver

--- ci/fats-cleanup.sh
+++ ci/fats-cleanup.sh
@@ -7,7 +7,9 @@ fats_dir=`dirname "${BASH_SOURCE[0]}"`/fats
 # attempt to cleanup fats
 if [ -d "$fats_dir" ]; then
   echo "Uninstall riff system"
-  duffle uninstall riff --credentials k8s || true
+  duffle_k8s_service_account=${duffle_k8s_service_account:-duffle-runtime}
+  duffle_k8s_namespace=${duffle_k8s_namespace:-kube-system}
+  SERVICE_ACCOUNT=${duffle_k8s_service_account} KUBE_NAMESPACE=${duffle_k8s_namespace} duffle uninstall riff -d k8s || true
   kubectl delete namespace $NAMESPACE || true
 
   source $fats_dir/cleanup.sh
