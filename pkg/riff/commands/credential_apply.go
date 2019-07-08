67d471311e23132fd01a46b46feadf9e29d5a1d9
Scott Andrews <sandrews@pivotal.io>
2019-07-08 13:19:15 -0400

    Apply line wrapped based on rendered output width

--- pkg/riff/commands/credential_apply.go
+++ pkg/riff/commands/credential_apply.go
@@ -148,18 +148,17 @@ func NewCredentialApplyCommand(ctx context.Context, c *cli.Config) *cobra.Comman
 Create or update credentials for a container registry.
 
 In addition to creating a credential, the default image prefix can be set by
-specifying ` + cli.SetDefaultImagePrefixFlagName + `. The prefix is applied to
-application and function builds in order to skip needing to specify a fully
-qualified image repository.
+specifying ` + cli.SetDefaultImagePrefixFlagName + `. The prefix is applied to application and
+function builds in order to skip needing to specify a fully qualified image
+repository.
 
 The default image prefix depends on the repository and take the form:
 - Docker Hub: docker.io/<docker-user-name>
 - GCR: gcr.io/<google-cloud-project-id>
 
-Other image prefix values may be defined by specifying
-` + cli.DefaultImagePrefixFlagName + `. Manually specifying the default image
-prefix is required to set the prefix when also manually specifying the registry
-hostname.
+Other image prefix values may be defined by specifying ` + cli.DefaultImagePrefixFlagName + `.
+Manually specifying the default image prefix is required to set the prefix when
+also manually specifying the registry hostname.
 
 While multiple credentials can be created in a single namespace, only a single
 default image prefix can be set.
