1a240e614363b1991eedde8858365c236ae7f9f1
Scott Andrews <sandrews@pivotal.io>
2019-10-05 23:47:12 -0400

    Strip FATS pods of limits on single node clusters

--- ci/fats-cleanup.sh
+++ ci/fats-cleanup.sh
@@ -7,6 +7,9 @@ fats_dir=`dirname "${BASH_SOURCE[0]}"`/fats
 # attempt to cleanup fats
 if [ -d "$fats_dir" ]; then
   echo "Uninstall riff system"
+  kubectl delete riff --all-namespaces --all
+  kubectl delete knative --all-namespaces --all
+
   kubectl delete namespace $NAMESPACE
   
   helm delete --purge riff
