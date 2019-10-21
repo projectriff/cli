708e68a4a25e4f101ed00c123baca94c78889a3f
Scott Andrews <sandrews@pivotal.io>
2019-10-21 12:33:31 -0400

    pass through --kubeconfig --namespace -n flags

--- pkg/riff/commands/custom_completion.go
+++ pkg/riff/commands/custom_completion.go
@@ -1,6 +1,33 @@
 package commands
 
 const bash_completion_func = `
+__riff_override_flag_list=(--kubeconfig --namespace -n)
+__riff_override_flags()
+{
+    local ${__riff_override_flag_list[*]##*-} two_word_of of var
+    for w in "${words[@]}"; do
+        if [ -n "${two_word_of}" ]; then
+            eval "${two_word_of##*-}=\"${two_word_of}=\${w}\""
+            two_word_of=
+            continue
+        fi
+        for of in "${__riff_override_flag_list[@]}"; do
+            case "${w}" in
+                ${of}=*)
+                    eval "${of##*-}=\"${w}\""
+                    ;;
+                ${of})
+                    two_word_of="${of}"
+                    ;;
+            esac
+        done
+    done
+    for var in "${__riff_override_flag_list[@]##*-}"; do
+        if eval "test -n \"\$${var}\""; then
+            eval "echo -n \${${var}}' '"
+        fi
+    done
+}
 
 __riff_list_namespaces()
 {
@@ -8,7 +35,7 @@ __riff_list_namespaces()
     template="{{ range .items  }}{{ .metadata.name }} {{ end }}"
     local kubectl_out
     # TODO decouple from kubectl
-    if kubectl_out=$(kubectl get -o template --template="${template}" namespace 2>/dev/null); then
+    if kubectl_out=$(kubectl get $(__riff_override_flags) -o template --template="${template}" namespace 2>/dev/null); then
         COMPREPLY=( $( compgen -W "${kubectl_out}[*]" -- "$cur" ) )
     fi
 }
@@ -19,7 +46,7 @@ __riff_list_knative_configurations()
     template="{{ range .items  }}{{ .metadata.name }} {{ end }}"
     local kubectl_out
     # TODO decouple from kubectl
-    if kubectl_out=$(kubectl get -o template --template="${template}" configurations.serving.knative.dev 2>/dev/null); then
+    if kubectl_out=$(kubectl get $(__riff_override_flags) -o template --template="${template}" configurations.serving.knative.dev 2>/dev/null); then
         COMPREPLY=( $( compgen -W "${kubectl_out}[*]" -- "$cur" ) )
     fi
 }
@@ -30,7 +57,7 @@ __riff_list_knative_services()
     template="{{ range .items  }}{{ .metadata.name }} {{ end }}"
     local kubectl_out
     # TODO decouple from kubectl
-    if kubectl_out=$(kubectl get -o template --template="${template}" services.serving.knative.dev 2>/dev/null); then
+    if kubectl_out=$(kubectl get $(__riff_override_flags) -o template --template="${template}" services.serving.knative.dev 2>/dev/null); then
         COMPREPLY=( $( compgen -W "${kubectl_out}[*]" -- "$cur" ) )
     fi
 }
@@ -41,7 +68,7 @@ __riff_list_streaming_provisioner_services()
     template="{{ range .items  }}{{ .metadata.name }} {{ end }}"
     local kubectl_out
     # TODO decouple from kubectl
-    if kubectl_out=$(kubectl get -o template --template="${template}" --selector streaming.projectriff.io/provisioner services 2>/dev/null); then
+    if kubectl_out=$(kubectl get $(__riff_override_flags) -o template --template="${template}" --selector streaming.projectriff.io/provisioner services 2>/dev/null); then
         COMPREPLY=( $( compgen -W "${kubectl_out}[*]" -- "$cur" ) )
     fi
 }
@@ -65,7 +92,7 @@ __riff_list_resource()
 {
 	__riff_debug "listing $1"
     local riff_output out
-    if riff_output=$(riff $1 2>/dev/null); then
+    if riff_output=$(riff $1 $(__riff_override_flags) 2>/dev/null); then
         out=($(echo "${riff_output}" | awk 'NR>1 {print $1}'))
         COMPREPLY=( $( compgen -W "${out[*]}" -- "$cur" ) )
     fi
