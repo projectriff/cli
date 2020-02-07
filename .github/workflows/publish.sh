412f73fd2360adb5f45b6f59d9ca714c34a93722
Scott Andrews <sandrews@pivotal.io>
2020-02-07 13:05:42 -0500

    fix publish script to handle new versions

--- .github/workflows/publish.sh
+++ .github/workflows/publish.sh
@@ -15,7 +15,7 @@ gcloud auth activate-service-account --key-file <(echo $GCLOUD_CLIENT_SECRET | b
 
 bucket=gs://projectriff/riff-cli/releases
 
-gsutil rsync -a public-read -d ${bucket}/builds/v${slug} ${bucket}/v${version}
+gsutil rsync -a public-read -d ${bucket}/builds/v${slug}/ ${bucket}/v${version}/
 if [[ "$git_branch" == "refs/heads/master" ]]; then
-  gsutil rsync -a public-read -d ${bucket}/builds/v${slug} ${bucket}/latest
+  gsutil rsync -a public-read -d ${bucket}/builds/v${slug}/ ${bucket}/latest/
 fi
