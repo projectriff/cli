a6444bd4619b82859fa6111ea034b2be98813cbe
Scott Andrews <sandrews@pivotal.io>
2020-03-30 09:45:27 -0400

    Adapt to cobra 0.0.7

--- pkg/build/commands/application_test.go
+++ pkg/build/commands/application_test.go
@@ -26,9 +26,8 @@ import (
 func TestApplicationCommand(t *testing.T) {
 	table := rifftesting.CommandTable{
 		{
-			Name:        "empty",
-			Args:        []string{},
-			ShouldError: true,
+			Name: "empty",
+			Args: []string{},
 		},
 	}
 
